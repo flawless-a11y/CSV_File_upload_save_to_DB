d5994ee2d73ab2767b5484b62f5ed7ac89142a67
Prashant chauhan <prashant2.chauhan@PPBNOMBPBT-278.local>
2022-04-20 11:56:57 +0530

    fixed the indentation issue in import.sql due to code formatting and added eureka clinet discovery

--- src/main/java/com/shopping/merchant/catalogue/MerchantCatalogueApplication.java
+++ src/main/java/com/shopping/merchant/catalogue/MerchantCatalogueApplication.java
@@ -3,9 +3,11 @@ package com.shopping.merchant.catalogue;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @OpenAPIDefinition
+@EnableEurekaClient
 public class MerchantCatalogueApplication {
 
     public static void main(String[] args) {
